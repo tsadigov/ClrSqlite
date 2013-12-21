c520d0633472e4061f70c6672960c887c518d231
tsadigov <tsadigov@yahoo.com>
2013-12-21 07:23:21 -0800

    refactor sqlliteInt moved  CharExtensions so that it sees all preprocessors

--- Community.CsharpSqlite/src/sqliteInt_h.cs
+++ Community.CsharpSqlite/src/sqliteInt_h.cs
@@ -42,71 +42,7 @@ namespace Community.CsharpSqlite
 
    
 
-    public partial class CharExtensions
-    {
-        static byte[] sqlite3CtypeMap
-        {
-            get
-            {
-                return Sqlite3.sqlite3CtypeMap;
-            }
-        }
-        /*
-    ** The following macros mimic the standard library functions toupper(),
-    ** isspace(), isalnum(), isdigit() and isxdigit(), respectively. The
-    ** sqlite versions only work for ASCII characters, regardless of locale.
-    */
-#if SQLITE_ASCII
-        //# define sqlite3Toupper(x)  ((x)&~(sqlite3CtypeMap[(unsigned char)(x)]&0x20))
 
-        //# define CharExtensions.sqlite3Isspace(x)   (sqlite3CtypeMap[(unsigned char)(x)]&0x01)
-        //private 
-        public  static bool sqlite3Isspace(byte x)
-        {
-            return (Sqlite3.sqlite3CtypeMap[(byte) (x)] & 0x01) != 0;
-        }
-
-        //private
-        public static bool sqlite3Isspace(char x)
-        {
-            return x < 256 && (sqlite3CtypeMap[(byte) (x)] & 0x01) != 0;
-        }
-
-        //# define sqlite3Isalnum(x)   (sqlite3CtypeMap[(unsigned char)(x)]&0x06)
-        public static bool sqlite3Isalnum(byte x)
-        {
-            return (sqlite3CtypeMap[(byte)(x)] & 0x06) != 0;
-        }
-        public static bool sqlite3Isalnum(char x)
-        {
-            return x < 256 && (sqlite3CtypeMap[(byte)(x)] & 0x06) != 0;
-        }
-
-        //# define sqlite3Isalpha(x)   (sqlite3CtypeMap[(unsigned char)(x)]&0x02)
-
-        //# define sqlite3Isdigit(x)   (sqlite3CtypeMap[(unsigned char)(x)]&0x04)
-        public static bool sqlite3Isdigit(byte x)
-        {
-            return (sqlite3CtypeMap[((byte)x)] & 0x04) != 0;
-        }
-        public static bool sqlite3Isdigit(char x)
-        {
-            return x < 256 && (sqlite3CtypeMap[((byte)x)] & 0x04) != 0;
-        }
-
-        //# define sqlite3Isxdigit(x)  (sqlite3CtypeMap[(unsigned char)(x)]&0x08)
-        public static bool sqlite3Isxdigit(byte x)
-        {
-            return (sqlite3CtypeMap[((byte)x)] & 0x08) != 0;
-        }
-        public static bool sqlite3Isxdigit(char x)
-        {
-            return x < 256 && (sqlite3CtypeMap[((byte)x)] & 0x08) != 0;
-        }
-
-        //# define sqlite3Tolower(x)   (sqlite3UpperToLower[(unsigned char)(x)])
-#endif
-    }
 
     public partial class Sqlite3
   {
@@ -3718,42 +3654,7 @@ static int SQLITE_MISUSE_BKPT() {return SQLITE_MISUSE;}
 static int SQLITE_CANTOPEN_BKPT() {return SQLITE_CANTOPEN;}
 #endif
 
-    /*
-** FTS4 is really an extension for FTS3.  It is enabled using the
-** SQLITE_ENABLE_FTS3 macro.  But to avoid confusion we also all
-** the SQLITE_ENABLE_FTS4 macro to serve as an alisse for SQLITE_ENABLE_FTS3.
-*/
-    //#if (SQLITE_ENABLE_FTS4) && !defined(SQLITE_ENABLE_FTS3)
-    //# define SQLITE_ENABLE_FTS3
-    //#endif
-
-    /*
-    ** The ctype.h header is needed for non-ASCII systems.  It is also
-    ** needed by FTS3 when FTS3 is included in the amalgamation.
-    */
-    //#if !defined(SQLITE_ASCII) || \
-    //    (defined(SQLITE_ENABLE_FTS3) && defined(SQLITE_AMALGAMATION))
-    //# include <ctype.h>
-    //#endif
-
-
-    /*
-    ** The following macros mimic the standard library functions toupper(),
-    ** isspace(), isalnum(), isdigit() and isxdigit(), respectively. The
-    ** sqlite versions only work for ASCII characters, regardless of locale.
-    */
-#if SQLITE_ASCII
-   
-
-#else
-//# define sqlite3Toupper(x)   toupper((unsigned char)(x))
-//# define CharExtensions.sqlite3Isspace(x)   isspace((unsigned char)(x))
-//# define sqlite3Isalnum(x)   isalnum((unsigned char)(x))
-//# define sqlite3Isalpha(x)   isalpha((unsigned char)(x))
-//# define sqlite3Isdigit(x)   isdigit((unsigned char)(x))
-//# define sqlite3Isxdigit(x)  isxdigit((unsigned char)(x))
-//# define sqlite3Tolower(x)   tolower((unsigned char)(x))
-#endif
+ 
 
     /*
 ** Internal function prototypes
@@ -4664,4 +4565,104 @@ static void IOTRACE( string X, params object[] ap ) { if ( SQLite3IoTrace ) { pr
     //#endif //* _SQLITEINT_H_ */
 
   }
+
+
+
+
+    public partial class CharExtensions
+    {
+        static byte[] sqlite3CtypeMap
+        {
+            get
+            {
+                return Sqlite3.sqlite3CtypeMap;
+            }
+        }
+
+
+        /*
+** FTS4 is really an extension for FTS3.  It is enabled using the
+** SQLITE_ENABLE_FTS3 macro.  But to avoid confusion we also all
+** the SQLITE_ENABLE_FTS4 macro to serve as an alisse for SQLITE_ENABLE_FTS3.
+*/
+        //#if (SQLITE_ENABLE_FTS4) && !defined(SQLITE_ENABLE_FTS3)
+        //# define SQLITE_ENABLE_FTS3
+        //#endif
+
+        /*
+        ** The ctype.h header is needed for non-ASCII systems.  It is also
+        ** needed by FTS3 when FTS3 is included in the amalgamation.
+        */
+        //#if !defined(SQLITE_ASCII) || \
+        //    (defined(SQLITE_ENABLE_FTS3) && defined(SQLITE_AMALGAMATION))
+        //# include <ctype.h>
+        //#endif
+
+
+        /*
+        ** The following macros mimic the standard library functions toupper(),
+        ** isspace(), isalnum(), isdigit() and isxdigit(), respectively. The
+        ** sqlite versions only work for ASCII characters, regardless of locale.
+        */
+#if SQLITE_ASCII
+
+
+        //# define sqlite3Toupper(x)  ((x)&~(sqlite3CtypeMap[(unsigned char)(x)]&0x20))
+
+        //# define CharExtensions.sqlite3Isspace(x)   (sqlite3CtypeMap[(unsigned char)(x)]&0x01)
+        //private 
+        public static bool sqlite3Isspace(byte x)
+        {
+            return (Sqlite3.sqlite3CtypeMap[(byte)(x)] & 0x01) != 0;
+        }
+
+        //private
+        public static bool sqlite3Isspace(char x)
+        {
+            return x < 256 && (sqlite3CtypeMap[(byte)(x)] & 0x01) != 0;
+        }
+
+        //# define sqlite3Isalnum(x)   (sqlite3CtypeMap[(unsigned char)(x)]&0x06)
+        public static bool sqlite3Isalnum(byte x)
+        {
+            return (sqlite3CtypeMap[(byte)(x)] & 0x06) != 0;
+        }
+        public static bool sqlite3Isalnum(char x)
+        {
+            return x < 256 && (sqlite3CtypeMap[(byte)(x)] & 0x06) != 0;
+        }
+
+        //# define sqlite3Isalpha(x)   (sqlite3CtypeMap[(unsigned char)(x)]&0x02)
+
+        //# define sqlite3Isdigit(x)   (sqlite3CtypeMap[(unsigned char)(x)]&0x04)
+        public static bool sqlite3Isdigit(byte x)
+        {
+            return (sqlite3CtypeMap[((byte)x)] & 0x04) != 0;
+        }
+        public static bool sqlite3Isdigit(char x)
+        {
+            return x < 256 && (sqlite3CtypeMap[((byte)x)] & 0x04) != 0;
+        }
+
+        //# define sqlite3Isxdigit(x)  (sqlite3CtypeMap[(unsigned char)(x)]&0x08)
+        public static bool sqlite3Isxdigit(byte x)
+        {
+            return (sqlite3CtypeMap[((byte)x)] & 0x08) != 0;
+        }
+        public static bool sqlite3Isxdigit(char x)
+        {
+            return x < 256 && (sqlite3CtypeMap[((byte)x)] & 0x08) != 0;
+        }
+
+        //# define sqlite3Tolower(x)   (sqlite3UpperToLower[(unsigned char)(x)])
+#else
+//# define sqlite3Toupper(x)   toupper((unsigned char)(x))
+//# define CharExtensions.sqlite3Isspace(x)   isspace((unsigned char)(x))
+//# define sqlite3Isalnum(x)   isalnum((unsigned char)(x))
+//# define sqlite3Isalpha(x)   isalpha((unsigned char)(x))
+//# define sqlite3Isdigit(x)   isdigit((unsigned char)(x))
+//# define sqlite3Isxdigit(x)  isxdigit((unsigned char)(x))
+//# define sqlite3Tolower(x)   tolower((unsigned char)(x))
+#endif
+    }
 }
